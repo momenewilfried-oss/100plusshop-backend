2bf886c0badd991b168375be01bd17b369f34f9b
momenewilfried-oss <momenewilfried@gmail.com>
2026-08-14 14:29:00 +0100

    Refactor categorie route to include validation and delete
    nwf

--- src/route/categorie.route.js
+++ src/route/categorie.route.js
@@ -2,14 +2,31 @@ const express = require('express');
 const router = express.Router();
 const verifierToken = require('../middleware/auth.middleware');
 const autoriserRoles = require('../middleware/role.middleware');
+const { validateRequest } = require('../validators/validateRequest');
+const categorieSchemas = require('../validators/categorie.validator');
 const {
   listerCategories,
   creerCategorie,
+  supprimerCategorie,
 } = require('../controllers/categorie.controller');
 
 router.use(verifierToken);
 
-router.get('/', autoriserRoles('administrateur', 'gerant', 'vendeur'), listerCategories);
-router.post('/', autoriserRoles('administrateur', 'gerant'), creerCategorie);
+router.get(
+  '/',
+  autoriserRoles('administrateur', 'gerant', 'vendeur'),
+  listerCategories
+);
+router.post(
+  '/',
+  autoriserRoles('administrateur', 'gerant'),
+  validateRequest(categorieSchemas.creer),
+  creerCategorie
+);
+router.delete(
+  '/:id',
+  autoriserRoles('administrateur', 'gerant'),
+  supprimerCategorie
+);
 
 module.exports = router;
