6814dd3a8e392fbc40bcb6500c19021e664366e3
momenewilfried-oss <momenewilfried@gmail.com>
2026-08-14 14:33:02 +0100

    Update marque.route.js
    nwr

--- src/route/marque.route.js
+++ src/route/marque.route.js
@@ -2,11 +2,31 @@ const express = require('express');
 const router = express.Router();
 const verifierToken = require('../middleware/auth.middleware');
 const autoriserRoles = require('../middleware/role.middleware');
-const { listerMarques, creerMarque } = require('../controllers/marque.controller');
+const { validateRequest } = require('../validators/validateRequest');
+const marqueSchemas = require('../validators/marque.validator');
+const {
+  listerMarques,
+  creerMarque,
+  supprimerMarque,
+} = require('../controllers/marque.controller');
 
 router.use(verifierToken);
 
-router.get('/', autoriserRoles('administrateur', 'gerant', 'vendeur'), listerMarques);
-router.post('/', autoriserRoles('administrateur', 'gerant'), creerMarque);
+router.get(
+  '/',
+  autoriserRoles('administrateur', 'gerant', 'vendeur'),
+  listerMarques
+);
+router.post(
+  '/',
+  autoriserRoles('administrateur', 'gerant'),
+  validateRequest(marqueSchemas.creer),
+  creerMarque
+);
+router.delete(
+  '/:id',
+  autoriserRoles('administrateur', 'gerant'),
+  supprimerMarque
+);
 
 module.exports = router;
